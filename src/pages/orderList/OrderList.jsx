23afb9bf163e69979dd2a88d12b1d14e2c935ab4
TranHuyWin <tranhuywin11b1@gmail.com>
2021-12-14 15:55:25 +0700

    first commit

---
+++ src/pages/orderList/OrderList.jsx
@@ -0,0 +1,72 @@
+import "./OrderList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { userRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function OrderList() {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="orderListUser">
+            <img className="orderListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction Volume",
+      width: 180,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 180,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="orderListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="orderListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="orderList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={10}
+        checkboxSelection
+      />
+    </div>
+  );
+}
